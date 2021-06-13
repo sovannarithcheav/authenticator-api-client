37657d4ef258d19f8dcc21921140f60b3def711e
CHEAV Sovannarith <cheavsovannarith@gmail.com>
2021-06-13 20:29:56 +0700

    enable feign client

--- src/main/kotlin/kh/org/sora/authentication_api/AuthenticationApiApplication.kt
+++ src/main/kotlin/kh/org/sora/authentication_api/AuthenticationApiApplication.kt
@@ -2,8 +2,10 @@ package kh.org.sora.authentication_api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class AuthenticationApiApplication
 
 fun main(args: Array<String>) {
